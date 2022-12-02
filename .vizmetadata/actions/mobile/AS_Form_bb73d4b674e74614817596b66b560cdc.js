b9c2cba8e1537a08060db86dec059b980e39d321
Sirisha Chowdary <bonthu.sirisha@hcl.com>
2022-12-02 10:33:40 +0530

    exportfromIris

--- .vizmetadata/actions/mobile/AS_Form_bb73d4b674e74614817596b66b560cdc.js
+++
@@ -1,3 +0,0 @@
-function AS_Form_bb73d4b674e74614817596b66b560cdc() {
-    return bottomScrollPostShow.call(this);
-}
---
+++ studioactions/mobile/ACTION_SEQUENCE_AS_h648b84b935a465fa33f2d20be69e5ae.js
@@ -0,0 +1,3 @@
+function ACTION_SEQUENCE_AS_h648b84b935a465fa33f2d20be69e5ae() {
+    return loginPostShow.call(this);
+}
